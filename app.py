64cba072b2ed43f7d651d8b8c73cdd47ffbab2d6
OviWahyu062 <129945441+OviWahyu062@users.noreply.github.com>
2026-09-16 10:22:55 +0700

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,66 @@
-
 import streamlit as st
 
-st.set_page_config(page_title="Sistem Master Material", layout="wide")
+from components.auth import login_user
+from pages.dashboard import dashboard
 
-st.title("Sistem Master Material & Approval")
-st.write("Prototype Streamlit - siap dikembangkan dengan Google Spreadsheet.")
+
+st.set_page_config(
+    page_title="Sistem Master Material",
+    page_icon="📦",
+    layout="wide"
+)
+
+
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+
+if st.session_state.login == False:
+
+    login_user()
+
+
+else:
+
+    st.sidebar.title(
+        "Sistem Master Material"
+    )
+
+
+    menu = st.sidebar.selectbox(
+        "Menu",
+        [
+            "Dashboard",
+            "Form Permintaan Material",
+            "Approval Workspace",
+            "Dokumen Persetujuan",
+            "Profile"
+        ]
+    )
+
+
+    if menu=="Dashboard":
+        dashboard()
+
+
+    elif menu=="Form Permintaan Material":
+
+        from pages.form_material import form_material
+        form_material()
+
+
+    elif menu=="Approval Workspace":
+
+        from pages.approval import approval
+        approval()
+
+
+    elif menu=="Dokumen Persetujuan":
+
+        from pages.document import document
+        document()
+
+
+    elif menu=="Profile":
+
+        from pages.profile import profile
